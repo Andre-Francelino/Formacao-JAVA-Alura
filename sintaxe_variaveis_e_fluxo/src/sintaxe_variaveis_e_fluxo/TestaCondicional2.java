a077bc95d8891c327be36229f71f19105d49898d
Andre Francelino <andrefgomes.github.version@gmail.com>
2026-06-24 11:52:34 -0300

    Aula 8: Testando Condicionais 2

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaCondicional2.java
@@ -0,0 +1,25 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaCondicional2 {
+	
+	public static void main(String[] args) {
+
+		System.out.println("===== Aula 8: Testando Condicionais 2 =====");
+
+		int idade = 27;
+		int quantidadePessoas = 5;
+		boolean acompanhado = quantidadePessoas >= 2;
+		
+		System.out.println("você tem " + idade + " anos.");
+		System.out.println("você(s) está(ão) em " + quantidadePessoas + " pessoa(s)");
+		System.out.println("valor de acompanhado é " + acompanhado);
+		
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Seja bem vindo!");
+		} else {
+			System.out.println("Infelizmente você não pode entrar");
+		}
+			 
+	}
+
+}
