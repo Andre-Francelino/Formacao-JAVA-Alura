9f7c378464f300aac3d72d593d19730825f81187
Andre Francelino <andrefgomes.github.version@gmail.com>
2026-07-12 22:17:17 -0300

    Testando For

---
+++ sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaFor.java
@@ -0,0 +1,15 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaFor {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("===== Aula 12 - Testando a estrutura de repetição For =====");
+		
+		for( int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+		}
+		
+	}
+
+}
